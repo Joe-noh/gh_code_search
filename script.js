32833eb50dbc1f1e748b6dd89b6973223e07a702
Joe-noh <goflb.jh@gmail.com>
2014-09-21 23:02:14 +0900

    worked

---
+++ script.js
@@ -0,0 +1,19 @@
+function create_tab(url) {
+    chrome.tabs.create({
+        "url": url
+    });
+}
+
+function build_url(lang, query) {
+    var q = query.split(/\s+/).map(encodeURIComponent).join("+");
+    var l = encodeURIComponent(lang);
+
+    return "https://github.com/search?type=Code&utf8=%E2%9C%93&l="+l+"&q="+q;
+}
+
+document.getElementById("submit-button").onclick = function () {
+    var lang  = document.getElementById("lang-input").value;
+    var query = document.getElementById("query-input").value;
+
+    create_tab(build_url(lang, query));
+}
